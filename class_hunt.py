131d009aacaf470551eaf3bfe49c6e0610f4286a
Mattéo Verdez <matteo.verdez@ynov.com>
2023-11-21 12:36:41 +0100

    [REM] debug lines

--- class_hunt.py
+++ class_hunt.py
@@ -8,9 +8,7 @@ class Hunt(Character):
         self._atk += int((self._atk * (15 / 100)))
         self._critrate += 30
         self._critdmg += int(self._critdmg * (20 / 100))
-        print(f"old : {old_atk}, current : {self._atk}ATK, CRIT DMG : {self._critdmg}%, CRIT RATE : {self._critrate}%")
         self.attack(who)
         self._atk = old_atk
         self._critdmg = old_critdmg
         self._critrate = old_critrate
-        print(f"here : {self._atk}")
