a1e44e330b012ca7fceaffcbe1afcc646c88701e
Mattéo Verdez <matteo.verdez@ynov.com>
2023-11-21 23:41:45 +0100

    Refactor ability boosts in Harmony class

--- class_harmony.py
+++ class_harmony.py
@@ -7,13 +7,10 @@ random.seed()
 class Harmony(Character):
     def buf_ability(self, target: Character):
         boost = random.randint(1, 3)
-        if boost == 1: # HP Boost
-            print("HPMAX BOOSTED")
-            target._maxhp += int((target._maxhp * (20 / 100)))
-        elif boost == 2: # DEF Boost
-            print("DEF BOOSTED")
-            target._def += int((target._def * (20 / 100)))
-        elif boost == 3: # ATK Boost
-            print("ATK BOOSTED")
-            target._atk += int((target._atk * (20 / 100)))
+        if boost == 1:
+            target._critdmg += 20
+        elif boost == 2:
+            target._critrate += 35
+        elif boost == 3:
+            target._atk += int((target._atk * (25 / 100)))
         pass
