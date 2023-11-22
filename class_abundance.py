07e9057556ef3ce0b17e4a711a72e42de43b4cd9
ahliko <killian.guillemot@ynov.com>
2023-11-22 16:43:51 +0100

    Patch heal + private attribut

--- class_abundance.py
+++ class_abundance.py
@@ -1,9 +1,18 @@
 from class_character import Character
 
+
 class Abundance(Character):
-    def heal(self, target : Character):
-        heal =  int(target._hp + (target._maxhp * (15/100)))
-        if target._maxhp < target._hp + heal:
-            target._hp = target._maxhp
-        else :
-            target._hp += heal
+    @staticmethod
+    def heal(target: Character):
+        heal = int(target.hp + (target.maxhp * (15 / 100)))
+        if target.maxhp <= heal:
+            target.hp = target.maxhp
+        else:
+            target.hp = heal
+
+
+if __name__ == "__main__":
+    abd = Abundance("Abundance", 500, 10, 10)
+    abd.hp = 250
+    abd.heal(abd)
+    print(abd.hp)
