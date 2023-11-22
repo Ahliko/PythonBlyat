574256402413247d5d68b9bc2d4e65a827a6f628
Logards <bastien.ricci@ynov.com>
2023-11-22 19:01:15 +0100

    update

--- class_preservation.py
+++ class_preservation.py
@@ -4,4 +4,3 @@ class Preservation(Character):
     def protection(self, target : Character):
         target._shield = int(target._shield + (target._maxhp * (15/100)))
         return self._shield
-    
