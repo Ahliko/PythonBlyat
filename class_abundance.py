75a7b35ba75713c528eb94c130a0f757f251b109
Logards <riccibastien@gmail.com>
2023-11-24 11:21:53 +0100

    ajout ultime

--- class_abundance.py
+++ class_abundance.py
@@ -10,6 +10,19 @@ class Abundance(Character):
         else:
             target.hp = heal
 
+    @staticmethod
+    def ultime(self, target : []):
+        heal = int(target.hp + (target.maxhp * (15 / 100) + 40))
+        if self.ultime >= 130:
+            for i in target:
+                if i.maxhp <= heal:
+                    i.hp = i.maxhp
+                else:
+                    i.hp = heal
+        else:
+            print("vous n'avez pas assez de points d'ultime")
+
+
 
 if __name__ == "__main__":
     abd = Abundance("Abundance", 500, 10, 10)
