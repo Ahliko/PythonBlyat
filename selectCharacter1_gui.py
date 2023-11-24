7afbfc598c80d3dd0f111b398bd2de604264376c
MartiFiak <jacolotmarine@gmail.com>
2023-11-24 12:47:27 +0100

    [Add] exitButton working + playButton working + add selectCharacter1(+2+3)_gui.py

---
+++ selectCharacter1_gui.py
@@ -0,0 +1,69 @@
+import pygame as pg
+from main_menu_gui import MainMenu
+import pygame_widgets
+from CustomButton import Button
+from CustomLabel import Label
+from Environnement_ecran import EnvironnementEcran
+
+
+class CharacterMenu1():
+    def __init__(self, previous_menu: MainMenu):
+        self.previous_menu = previous_menu
+        self.settings_menu = None
+        self.__quit = False
+
+    def disable(self):
+        self.__quit = True
+        pg.event.wait(100)
+
+    def update_screen(self, **kwargs):
+        self.previous_menu.ecran.blit(self.previous_menu.background, (0, 0))
+        for i in kwargs:
+            kwargs[i].draw(self.previous_menu.ecran)
+
+    def on_click_next(self):
+        print("Next")
+    def on_click_back(self):
+        print("Back")
+    def on_click_choice1(self):
+        print("you choose 1")
+    def on_click_choice2(self):
+        print("you choose 2")
+    def on_click_choice3(self):
+        print("you choose 3")
+
+    def update_screen(self, **kwargs):
+        self.previous_menu.ecran.blit(self.previous_menu.background, (0, 0))
+        for i in kwargs:
+            kwargs[i].draw(self.previous_menu.ecran)
+
+    def run(self):
+        pg.display.set_caption('PythonBlyat - SelectCharacter1')
+        bouton_next = Button((self.previous_menu.largeur / 2) + 615, (self.previous_menu.hauteur / 2) + 360, 130, 40, self.previous_menu.font, 'Next',
+                             self.on_click_next, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_back = Button((self.previous_menu.largeur / 2) - 745, (self.previous_menu.hauteur / 2) + 360, 130, 40, self.previous_menu.font, 'Back',
+                             self.on_click_back, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_choice1 = Button((self.previous_menu.largeur / 2) - 450, (self.previous_menu.hauteur / 2), 130, 40, self.previous_menu.font, 'Choice1',
+                             self.on_click_choice1, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_choice2 = Button((self.previous_menu.largeur / 2) - 60, (self.previous_menu.hauteur / 2), 130, 40, self.previous_menu.font, 'Choice2',
+                                self.on_click_choice2, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_choice3 = Button((self.previous_menu.largeur / 2) + 350, (self.previous_menu.hauteur / 2), 130, 40, self.previous_menu.font, 'Choice3',
+                                self.on_click_choice3, False, ('#2a75a1', '#666666', '#333333'))
+        label_title = Label("PythonBlyat", 100, (0, 0, 0), (self.previous_menu.largeur / 2, self.previous_menu.hauteur / 2 - 300), None, True)
+        label_selection = Label("Choose your character1", 50, (0, 0, 0), (self.previous_menu.largeur / 2, self.previous_menu.hauteur / 2 - 150),
+                                None, True)
+        self.update_screen(next=bouton_next, back=bouton_back, choice1=bouton_choice1, choice2=bouton_choice2, choice3=bouton_choice3, title=label_title, text=label_selection)
+        while not self.__quit:
+            self.previous_menu.clock.tick(self.previous_menu.framerate)
+            events = pg.event.get()
+            for event in events:
+                if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                    print("Touche Echap")
+                    pg.quit()
+                    exit()
+                elif event.type == pg.QUIT:
+                    pg.quit()
+                    exit()
+            self.update_screen(next=bouton_next, back=bouton_back, choice1=bouton_choice1, choice2=bouton_choice2, choice3=bouton_choice3, title=label_title, text=label_selection)
+            pygame_widgets.update(events)
+            pg.display.update()
--- selectCharacter3_gui.py
+++ selectCharacter3_gui.py
@@ -1,10 +1,11 @@
 import pygame as pg
+from main_menu_gui import MainMenu
 from CustomButton import Button
 from CustomLabel import Label
 from Environnement_ecran import EnvironnementEcran
 
 
-class MainMenu(EnvironnementEcran):
+class CharacterMenu3():
     def __init__(self):
         super().__init__(1920, 1080, (255, 255, 255), 60)
         self.__quit = False
@@ -34,17 +35,16 @@ class MainMenu(EnvironnementEcran):
         self.ecran.blit(self.texte, self.texte_rect)
         pg.display.flip()
 
-    def on_click_play(self):
-        from selectCharacter1_gui import CharacterMenu1
-        character1_menu = CharacterMenu1(self)
-        character1_menu.run()
-
-    def on_click_settings(self):
-        from settings_gui import SettingsMenu
-        settings_menu = SettingsMenu(self)
-        settings_menu.run()
-    def on_click_exit(self):
-        self.__quit = True
+    def on_click_next(self):
+        print("Next")
+    def on_click_back(self):
+        print("Back")
+    def on_click_choice1(self):
+        print("you choose 1")
+    def on_click_choice2(self):
+        print("you choose 2")
+    def on_click_choice3(self):
+        print("you choose 3")
 
     def update_screen(self, **kwargs):
         self.ecran.blit(self.background, (0, 0))
@@ -54,15 +54,20 @@ class MainMenu(EnvironnementEcran):
     def run(self):
         pg.font.init()
         self.change_font("Arial", 30)
-        pg.display.set_caption('PythonBlyat - MainMenu')
-        bouton_play = Button((self.largeur / 2) - 100, self.hauteur / 2, 200, 50, self.font, 'Play',
-                             self.on_click_play, False, ('#2a75a1', '#666666', '#333333'))
-        bouton_settings = Button((self.largeur / 2) - 100, (self.hauteur / 2) + 60, 200, 50, self.font, 'Settings',
-                                 self.on_click_settings, False, ('#2a75a1', '#666666', '#333333'))
-        bouton_exit = Button((self.largeur / 2) - 100, (self.hauteur / 2) + 120, 200, 50, self.font, 'Exit',
-                             self.on_click_exit, False, ('#2a75a1', '#666666', '#333333'))
-        label_title = Label("PythonBlyat", 100, (0, 0, 0), (self.largeur / 2, self.hauteur / 2 - 50), None, True)
-        self.update_screen(play=bouton_play, settings=bouton_settings, quit=bouton_exit, title=label_title)
+        pg.display.set_caption('PythonBlyat - SelectCharacter3')
+        bouton_next = Button((self.largeur / 2) + 615, (self.hauteur / 2) + 360, 130, 40, self.font, 'Next',
+                             self.on_click_next, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_back = Button((self.largeur / 2) - 745, (self.hauteur / 2) + 360, 130, 40, self.font, 'Back',
+                             self.on_click_back, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_choice1 = Button((self.largeur / 2) - 450, (self.hauteur / 2), 130, 40, self.font, 'Choice1',
+                             self.on_click_choice1, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_choice2 = Button((self.largeur / 2) - 60, (self.hauteur / 2), 130, 40, self.font, 'Choice2',
+                                self.on_click_choice2, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_choice3 = Button((self.largeur / 2) + 350, (self.hauteur / 2), 130, 40, self.font, 'Choice3',
+                                self.on_click_choice3, False, ('#2a75a1', '#666666', '#333333'))
+        label_title = Label("PythonBlyat", 100, (0, 0, 0), (self.largeur / 2, self.hauteur / 2 - 300), None, True)
+        label_selection = Label("Choose your character3", 50, (0, 0, 0), (self.largeur / 2, self.hauteur / 2 - 150), None, True)
+        self.update_screen(next=bouton_next, back=bouton_back, choice1=bouton_choice1, choice2=bouton_choice2, choice3=bouton_choice3, title=label_title, text=label_selection)
         pg.display.flip()
         while not self.__quit:
             self.clock.tick(self.framerate)
@@ -72,7 +77,7 @@ class MainMenu(EnvironnementEcran):
                     self.__quit = True
                 elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                     self.__quit = True
-            self.update_screen(play=bouton_play, settings=bouton_settings, quit=bouton_exit, title=label_title)
+            self.update_screen(next=bouton_next, back=bouton_back, choice1=bouton_choice1, choice2=bouton_choice2, choice3=bouton_choice3, title=label_title, text=label_selection)
             pg.display.update()
         pg.quit()
         exit()
