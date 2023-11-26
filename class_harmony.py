213c68ab8147526f142d9a121c916ea0ceb02f7b
ahliko <killian.guillemot@ynov.com>
2023-11-26 11:47:53 +0100

    Reformat File + Patch attribut name + method -> static method

--- class_harmony.py
+++ class_harmony.py
@@ -4,12 +4,14 @@ from class_character import Character
 
 random.seed()
 
+
 class Harmony(Character):
-    def buf_ability(self, target: Character):
+    @staticmethod
+    def buf_ability(target: Character):
         boost = random.randint(1, 3)
         if boost == 1:
-            target._critdmg += 20
+            target.critdmg += 20
         elif boost == 2:
-            target._critrate += 35
+            target.critrate += 35
         elif boost == 3:
-            target._atk += int((target._atk * (25 / 100)))
+            target.atk += int((target.atk * (25 / 100)))
