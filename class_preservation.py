eaa609ee711e23a8b2c12b2911698973539ae765
ahliko <killian.guillemot@ynov.com>
2023-11-26 12:43:11 +0100

    Reformat File + Rename attribut use

--- class_preservation.py
+++ class_preservation.py
@@ -1,20 +1,17 @@
 from class_character import Character
 
-class Preservation(Character):
-    def protection(self, target : Character):
-        target._shield = int(target._shield + (target._maxhp * (15/100)))
-        self._ultime += 20
-        return self._shield
 
-    def ultime(self, target : []):
-        if self._ultime >= 150:
+class Preservation(Character):
+    def protection(self, target: Character):
+        target.shield = int(target.shield + (target.maxhp * (15 / 100)))
+        self.ultime += 20
+        return self.shield
+
+    def use_ultime(self, target: []):
+        if self.ultime >= 150:
             for i in target:
-                i._shield = int(i._shield + (i._def * (40/100) + 20 ))
-        else :
+                i.shield = int(i.shield + (i._def * (40 / 100) + 20))
+        else:
             print("Vous n'avez pas assez de points d'ultime")
             return
-        self._ultime = 0
-        
-
-                
-
+        self.ultime = 0
