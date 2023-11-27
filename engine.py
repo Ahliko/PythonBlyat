9dc3a26d0e28340021e4875912263ee8a732bfb2
ahliko <killian.guillemot@ynov.com>
2023-11-27 09:15:53 +0100

    Debug code

--- engine.py
+++ engine.py
@@ -1,39 +1,41 @@
 from __future__ import annotations
 from class_character import Character
 
-class Engine:
 
+class Engine:
     def __init__(self) -> None:
-        self.__turng : int = 1
-    
-    def next_character(list : []):
-        for charac in list:
-            if charac.turn == True:
+        self.__turng: int = 1
+
+    def next_character(self, lst: list):
+        for charac in lst:
+            if charac.turn:
                 return charac.name
-    
-    def next_turn(self, list : []):
+        self.next_turn(lst)
+
+    def next_turn(self, lst: list):
         occ = 0
-        for charac in list:
-            if charac.turn == False:
+        for charac in lst:
+            if charac.turn:
                 occ += 1
-        if occ == len(list):
-            self.__turn += 1
-            for charac in list:
+        if occ == len(lst):
+            self.__turng += 1
+            for charac in lst:
                 charac.turn = True
-        else :
-            self.next_perso(list)
+        else:
+            self.next_character(lst)
 
     @property
     def turng(self):
         return self.__turng
-    
+
     @turng.setter
-    def turn(self, amount):
+    def turng(self, amount):
         self.__turng = amount
 
 
 Lelfe = Character("CC", 100, 10, 122)
 Lelfe.turn = False
 Nain = Character("GH", 100, 122, 1)
-print(Engine.next_character([Lelfe, Nain]))
-
+Nain.turn = False
+engine = Engine()
+print(engine.next_character([Lelfe, Nain]))
