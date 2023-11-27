b029099672a792320e2ff205ebf6ef9fb40742c5
unknown <ecwanaps@gmail.com>
2023-11-27 18:46:43 +0100

    [FIX] ultime

--- class_abundance.py
+++ class_abundance.py
@@ -1,30 +1,21 @@
 from class_character import Character
 
-
 class Abundance(Character):
     @staticmethod
-    def heal(target: Character):
+    def ability(target: Character):
         heal = int(target.hp + (target.maxhp * (15 / 100)))
         if target.maxhp <= heal:
             target.hp = target.maxhp
         else:
             target.hp = heal
 
-    @staticmethod
-    def use_ultime(self, target: []):
-        heal = int(target.hp + (target.maxhp * (15 / 100) + 40))
-        if self.ultime >= 130:
+    def ultime(self, target: []):
+        if self.ultpts == self.maxultpts:
             for i in target:
+                heal = int(i.hp + (i.maxhp * (15 / 100) + 40))
                 if i.maxhp <= heal:
                     i.hp = i.maxhp
                 else:
                     i.hp = heal
         else:
-            print("vous n'avez pas assez de points d'ultime")
-
-
-if __name__ == "__main__":
-    abd = Abundance("Abundance", 500, 10, 10)
-    abd.hp = 250
-    abd.heal(abd)
-    print(abd.hp)
+            print("Vous n'avez pas assez de points d'ultime")
