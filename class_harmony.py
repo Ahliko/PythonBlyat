f16cc8d9b44c39c140cf052f76beb89d92df2852
unknown <ecwanaps@gmail.com>
2023-11-27 13:03:47 +0100

    [ADD] ultime

--- class_harmony.py
+++ class_harmony.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 import random
 from class_character import Character
 
+from class_hunt import Hunt
+
 random.seed()
 
 
 class Harmony(Character):
+
     @staticmethod
-    def buf_ability(target: Character):
+    def ability(target: Character):
         boost = random.randint(1, 3)
         if boost == 1:
             target.critdmg += 20
@@ -15,3 +18,16 @@ class Harmony(Character):
             target.critrate += 35
         elif boost == 3:
             target.atk += int((target.atk * (25 / 100)))
+
+    def ultime(self, target : []):
+        if self.ultpts == self.maxultpts:
+            print("UTILISATION DE LULTIME")
+            for i in target:
+                if i.is_alive:
+                    i.atk += int(i.atk * (25 / 100))
+                    i.critrate += 30
+                    i.critdmg += 15
+                    i.buf["remaining"] = 2
+        else:
+            print("Vous n'avez pas assez de points d'ultime")
+
