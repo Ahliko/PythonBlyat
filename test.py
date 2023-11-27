aab9257f62b3c9b4521eaa585a41c2dc55918de4
ahliko <killian.guillemot@ynov.com>
2023-11-27 20:00:49 +0100

    Finishing test

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ class Test:
     def __init__(self):
         self.screen = pygame.display.set_mode((1920, 1080))
         pygame.display.set_caption('PythonBlyat - Test')
+        self.clock = pygame.time.Clock()
 
         tmx_data = pytmx.util_pygame.load_pygame("carte.tmx")
         map_data = pyscroll.data.TiledMapData(tmx_data)
@@ -16,37 +17,98 @@ class Test:
         player_position = tmx_data.get_object_by_name("Player")
         self.player = Player(player_position.x, player_position.y)
 
+        self.walls = [pygame.Rect(obj.x, obj.y, obj.width, obj.height) for obj in tmx_data.objects if
+                      obj.type == "wall"]
+
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.center(self.player.rect.center)
         self.group.add(self.player)
 
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+            self.player.change_animation('up')
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+            self.player.change_animation('down')
+        if pressed[pygame.K_LEFT]:
+            self.player.move_left()
+            self.player.change_animation('left')
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+            self.player.change_animation('right')
+
+    def update(self):
+        self.group.update()
+        for sprite in self.group.sprites():
+            if sprite.feet.collidelist(self.walls) > -1:
+                sprite.move_back()
+
     def run(self):
-        self.group.center((0, 0))
         while True:
+            self.player.save_location()
+            self.handle_input()
+            self.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
-            self.group.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+            self.clock.tick(60)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load("WarriorDownDeath.png")
+        self.sprite_sheet = pygame.image.load("test.png")
         self.image = self.get_image(0, 0)
         self.rect = self.image.get_rect()
         self.position = [x, y]
+        self.speed = 2
+        self.images = {
+            'down': self.get_image(0, 0),
+            'left': self.get_image(0, 128 * 3),
+            'right': self.get_image(0, 128),
+            'up': self.get_image(0, 128 * 2)
+        }
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
+
+    def save_location(self):
+        self.old_position = self.position.copy()
+
+    def change_animation(self, name):
+        self.image = self.images[name]
+
+    def move_right(self):
+        self.position[0] += self.speed
+
+    def move_left(self):
+        self.position[0] -= self.speed
+
+    def move_up(self):
+        self.position[1] -= self.speed
+
+    def move_down(self):
+        self.position[1] += self.speed
 
     def update(self):
         self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def move_back(self):
+        self.position = self.old_position.copy()
+        self.update()
 
     def get_image(self, x, y):
-        image = pygame.Surface([32, 32])
-        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        image = pygame.Surface([64, 128])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 64, 128))
         image.set_colorkey((0, 0, 0))
         return image
+
 
 if __name__ == '__main__':
     pygame.init()
