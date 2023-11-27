0b26841b4ec2e144de16d7e611dec7885c7f8d6d
MartiFiak <jacolotmarine@gmail.com>
2023-11-27 09:27:45 +0100

    Add fight_gui.py

---
+++ fight_gui.py
@@ -0,0 +1,70 @@
+import pygame as pg
+from CustomButton import Button
+from CustomLabel import Label
+from Environnement_ecran import EnvironnementEcran
+
+
+class MainMenu(EnvironnementEcran):
+    def __init__(self):
+        super().__init__(1920, 1080, (255, 255, 255), 60)
+        self.__quit = False
+        self.texte_rect = None
+        self.texte = None
+        self.font = None
+        self.__background = pg.image.load("Zboulbizboulba.png")
+        self.__surface = pg.Surface((200, 200))
+
+    @property
+    def background(self):
+        return self.__background
+
+    def change_font(self, font: str, taille: int):
+        self.font = pg.font.SysFont(font, taille)
+
+    def on_click_attacks(self):
+        print("choice 1 : Acid pee attack")
+        print("choice 2 : 360 Attack of Death That Kills")
+        print("choice 3 : Stinky fart attack")
+        print("choice 4 : Just fart fat")
+
+    def on_click_items(self):
+        print("What in baaaaag ?")
+
+    def on_click_escape(self):
+        print("You have activated the special ability \"To take one's heels\", you run away like a coward...")
+
+    def update_screen(self, **kwargs):
+        self.ecran.blit(self.background, (0, 0))
+        for i in kwargs:
+            kwargs[i].draw(self.ecran)
+
+    def run(self):
+        pg.font.init()
+        self.change_font("Arial", 30)
+        pg.display.set_caption('PythonBlyat - Fight')
+        bouton_attack_de_base = Button((self.largeur / 2) - 400, (self.hauteur / 2) + 180, 200, 50, self.font, 'Attack de base',
+                                self.on_click_attacks, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_competences = Button((self.largeur / 2) - 400, (self.hauteur / 2) + 240, 200, 50, self.font, 'Compétences',
+                              self.on_click_items, False, ('#2a75a1', '#666666', '#333333'))
+        bouton_ultime = Button((self.largeur / 2) - 400, (self.hauteur / 2) + 300, 200, 50, self.font, 'Ultime',
+                               self.on_click_escape, False, ('#2a75a1', '#666666', '#333333'))
+
+        self.update_screen(attack=bouton_attack_de_base, competences=bouton_competences, ultime=bouton_ultime)
+        pg.display.flip()
+        while not self.__quit:
+            self.clock.tick(self.framerate)
+            events = pg.event.get()
+            for event in events:
+                if event.type == pg.QUIT:
+                    self.__quit = True
+                elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                    self.__quit = True
+            self.update_screen(attack=bouton_attack_de_base, competences=bouton_competences, ultime=bouton_ultime)
+            pg.display.update()
+        pg.quit()
+        exit()
+
+
+if __name__ == "__main__":
+    menu = MainMenu()
+    menu.run()
